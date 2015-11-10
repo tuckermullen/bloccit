9b9728569ef23408b56b9623c3d21995e791183f
tuckermullen <mullentucker@gmail.com>
2015-11-09 19:40:18 -0800

    Adjusted to Blocs solution

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
 
-  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body") }
-  let(:answer) { Answer.create!(question: question, body: 'Answer Body') }
+  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body", resolved: false) }
+  let(:answer) { Answer.create!(question: question, body: 'New Answer Body') }
 
   describe "attributes" do
     it "should respond to body" do
