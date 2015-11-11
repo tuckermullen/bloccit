8d9a00d6bf7aa2142d8f581ef1e5d54670fe0eb2
tuckermullen <mullentucker@gmail.com>
2015-11-10 17:19:15 -0800

    fixed a spec

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
 
-  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body", resolved: "New Question Resolved") }
+  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body", resolved: false) }
 
   describe "attributes" do
 
     it "should respond to title" do
       expect(question).to respond_to(:title)
     end
+
     it "should respond to body" do
       expect(question).to respond_to(:body)
     end
