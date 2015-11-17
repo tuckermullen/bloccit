3ef87d27e7f5e9235b8ded00b4a340a603d21e0f
tuckermullen <mullentucker@gmail.com>
2015-11-17 15:34:57 -0800

    Currently working on checkpoint 34

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
+include RandomData
 
 RSpec.describe Post, type: :model do
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
-  let(:post) {Post.create!(title: "New Post Title", body: "New Post Body") }
+  it { should belong_to(:topic) }
 
   describe "attribute" do
 
