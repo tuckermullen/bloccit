b69f8ab4e5ed21f5bc4010d00ffbeafca9898049
tuckermullen <mullentucker@gmail.com>
2015-11-17 10:03:47 -0800

    Attempt at assignment 33

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110015856) do
+ActiveRecord::Schema.define(version: 20151117173840) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20151110015856) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
