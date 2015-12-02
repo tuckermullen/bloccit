a3d908dc8e435c78d5aef27c1064fe680ce75110
Tucker Mullen <mullentucker@gmail.com>
2015-12-01 18:16:27 -0800

    In progress 12/1

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,7 +15,7 @@ class QuestionsController < ApplicationController
     @question = Question.new
     @question.title = params[:question][:title]
     @question.body = params[:question][:body]
-    # @question.resolved = params[:question][:resolved]
+    @question.resolved = params[:question][:resolved]
 
     if question.save
       flash[:notice] = "Question was saved."
@@ -36,7 +36,7 @@ class QuestionsController < ApplicationController
     @question.body = params[:question][:body]
     @question.resolved = params[:question][:resolved]
 
-    if question.save
+    if @question.save
       flash[:notice] = "Question was updated."
       redirect_to @question
     else
