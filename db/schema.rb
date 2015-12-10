5e54b99242ec2612d4c4cb685c61b62f9944c111
tuckermullen <mullentucker@gmail.com>
2015-12-10 13:40:13 -0800

    Fixed an error

--- db/schema.rb
+++ db/schema.rb
@@ -32,14 +32,6 @@ ActiveRecord::Schema.define(version: 20151209214740) do
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
 
-  create_table "questions", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.boolean  "resolved"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "sponsored_posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
