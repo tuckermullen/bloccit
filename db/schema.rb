b5214c11a7b1d2666752d3e58e71216b864dccff
tuckermullen <mullentucker@gmail.com>
2015-12-10 13:42:34 -0800

    Fixed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151113193226) do
+ActiveRecord::Schema.define(version: 20151209214740) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -35,6 +35,34 @@ ActiveRecord::Schema.define(version: 20151113193226) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "topic_id"
+  end
+
+  add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
+
+  create_table "sponsored_posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "price"
+    t.integer  "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "public",      default: true
+    t.text     "description"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
