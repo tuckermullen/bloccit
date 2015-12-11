3f181f4a2dd6419ad1911dd32c01cb872c53fc9a
tuckermullen <mullentucker@gmail.com>
2015-12-11 09:37:30 -0800

    Fixed some errors

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   resources :topics do
     resources :posts, except: [:index]
     resources :sponsored_posts
+    # get '/topics/:topic_id/sponsored_posts', => 'sponsored_posts#index'
+    # get '/topics/:topic_id/sponsored_posts/:id', => 'sponsored_posts#show'
   end
+  # get '/topics', => 'topics#index'
+  # get '/topics/:id', => 'topics#show'
 
   get 'about' => 'welcome#about'
 
