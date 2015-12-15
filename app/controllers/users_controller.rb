9c8175cdfedb94ddc6fbe5837dabd97949f9b2ca
tuckermullen <mullentucker@gmail.com>
2015-12-14 16:21:13 -0800

    Updated to Blocs solution

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,14 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def confirm
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+  end
+
   def create
     @user = User.new
     @user.name = params[:user][:name]
@@ -17,15 +25,6 @@ class UsersController < ApplicationController
       flash.now[:alert] = "There was an error creating your account. Please try again."
       render :new
     end
-  end
-
-  def confirm
-    @user = User.new
-    @user.name = params[:user][:name]
-    @user.email = params[:user][:email]
-    @user.password = params[:user][:password]
-    @user.password_confirmation = params[:user][:password_confirmation]
-    #How do i send the params has back to the 'create' action?
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create]
-
   post 'users/confirm' => 'users#confirm'
 
   get 'about' => 'welcome#about'
