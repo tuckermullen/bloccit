c274c96023bce14dadff0eb6eb98311f868ee1ba
tuckermullen <mullentucker@gmail.com>
2015-12-15 19:12:51 -0800

    Fixed 2 errors

--- app/models/sponsored_post.rb
+++ app/models/sponsored_post.rb
@@ -1,4 +1,11 @@
 class SponsoredPost < ActiveRecord::Base
   belongs_to :topic
-  has_many :comments, dependent: :destroy
+  # has_many :comments, dependent: :destroy
+
+  # def comments
+  #   Comment.where(sponsored_post_id: self.id)
+  # end
 end
+
+# @sponsored_post = SponsoredPost.first
+# @sponsored_post.comments
