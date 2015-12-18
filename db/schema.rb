202bd2344929709439fa810c0a09f664eea8235c
tuckermullen <mullentucker@gmail.com>
2015-12-18 10:11:39 -0800

    Fixed schema issues

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209214740) do
+ActiveRecord::Schema.define(version: 20151215235303) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -28,15 +28,16 @@ ActiveRecord::Schema.define(version: 20151209214740) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
+    t.integer  "user_id"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
-  create_table "sponsored_posts", force: :cascade do |t|
+  create_table "questions", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.integer  "price"
-    t.integer  "topic_id"
+    t.boolean  "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
