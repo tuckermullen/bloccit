91a11d46ccedc6d8047cac2de15a03e17c744f1a
tuckermullen <mullentucker@gmail.com>
2015-12-18 17:03:53 -0800

    In progess

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151215235303) do
+ActiveRecord::Schema.define(version: 20151218225923) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20151215235303) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
   it { should have_many(:posts)}
-  
+
   # Shoulda tests for name
   it { should validate_presence_of(:name) }
   it { should validate_length_of(:name).is_at_least(1) }
@@ -25,8 +25,51 @@ RSpec.describe User, type: :model do
       expect(user).to respond_to(:name)
     end
 
+    it "should respond to role" do
+      expect(user).to respond_to(:role)
+    end
+
+      it "should respond to admin?" do
+        expect(user).to respond_to(:admin?)
+      end
+
+      it "should respond to member?" do
+        expect(user).to respond_to(:member?)
+      end
+
     it "should respond to email" do
       expect(user).to respond_to(:email)
+    end
+  end
+
+  describe "roles" do
+
+    it "should be member by default" do
+      expect(user.role).to eql("member")
+    end
+
+    context "member user" do
+      it "should return true for #member?" do
+        expect(user.member?).to be_truthy
+      end
+
+      it "should return false for #admin?" do
+        expect(user.admin?).to be_falsey
+      end
+    end
+
+    context "admin user" do
+      before do
+        user.admin!
+      end
+
+      it "should return false for #member?" do
+        expect(user.member?).to be_falsey
+      end
+
+      it "should return true for #admin?" do
+        expect(user.admin?).to be_truthy
+      end
     end
   end
 
