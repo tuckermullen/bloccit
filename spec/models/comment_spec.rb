fb65bf7f7fa7d9748d3657d3c119062c8083d593
tuckermullen <mullentucker@gmail.com>
2015-12-19 16:05:57 -0800

    Checkpoint 41 complete

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,12 +2,17 @@ require 'rails_helper'
 include RandomData
 
 RSpec.describe Comment, type: :model do
-  describe "attributes" do
     let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
     let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.io", password: "helloworld") }
     let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
-    let(:comment) { Comment.create!(body: 'Comment Body', post: post) }
+    let(:comment) { Comment.create!(body: 'Comment Body', post: post, user: user) }
 
+    it { should belong_to(:post) }
+    it { should belong_to(:user) }
+    it { should validate_presence_of(:body) }
+    it { should validate_length_of(:body).is_at_least(5) }
+
+  describe "attributes" do
     it "should respond to body" do
       expect(comment).to respond_to(:body)
     end
