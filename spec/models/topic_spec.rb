ff4cd8b40fc74c6c77e739cbd82625a0a75bdc34
tuckermullen <mullentucker@gmail.com>
2015-12-30 19:25:24 -0800

    Assignment 46 complete

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -43,5 +43,17 @@ RSpec.describe Topic, type: :model do
         expect(Topic.visible_to(nil)).to eq([@public_topic])
       end
     end
+
+    describe "publicly_viewable(user)" do
+      it "shows topics that are viewable by all users" do
+        expect(Topic.publicly_viewable).to eq([@public_topic])
+      end
+    end
+
+    describe "privately_viewable(user)" do
+      it "only shows topics to users who are signed in" do
+        expect(Topic.privately_viewable).to eq([@private_topic])
+      end
+    end
   end
 end
