4fd4f007a4ca7f89a02ac509b26d513db0555910
tuckermullen <mullentucker@gmail.com>
2016-01-05 14:54:02 -0800

    Attempt at assignment

--- spec/api/v1/controllers/posts_controller_spec.rb
+++ spec/api/v1/controllers/posts_controller_spec.rb
@@ -39,4 +39,67 @@ RSpec.describe Api::V1::PostsController, type: :controller do
       expect(response).to have_http_status(403)
     end
   end
+
+  context "authenticated and authorized users" do
+    before do
+      my_user.admin!
+      controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
+      @new_post = build(:post)
+    end
+
+    describe "PUT update" do
+      before { put :update, topic_id: my_topic.id, id: my_post.id, post: {title: @new_post.title, body: @new_post.body} }
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "returns json content type" do
+        expect(response.content_type).to eq 'application/json'
+      end
+
+      it "updates a post with the correct attributes" do
+        updated_post = Post.find(my_topic.id, :post)
+        expect(updated_post.to_json).to eq response.body
+      end
+    end
+
+    describe "POST create" do
+      before { post :create, post: {title: @new_post.title, body: @new_post.body} }
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+    end
+
+    it "returns json content type" do
+      expect(response.content_type).to eq 'application/json'
+    end
+
+    it "creates a post with the correct attributes" do
+      hashed_json = JSON.parse(response.body)
+      expect(@new_post.title).to eq hashed_json["title"]
+      expect(@new_post.body).to eq hashed_json["body"]
+    end
+  end
+
+    describe "DELETE destroy" do
+      before { delete :destroy, topic_id: my_topic.id, id: my_post.id }
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "returns json content type" do
+        expect(response.content_type).to eq 'application/json'
+      end
+
+      it "returns the correct json success message" do
+        expect(response.body).to eq({"message" => "Post destroyed","status" => 200}.to_json)
+      end
+
+      it "deletes my_post" do
+        expect { Post.find(topic_id: my_topic.id, id: my_post.id) }.to raise_exception(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
 end
